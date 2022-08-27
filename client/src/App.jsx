242e6c65cad9ca27b1d565875d23f5cc1ec35833
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-27 14:27:27 +0100

    :art: Structure for the project
    Setting up the file structure and initializing some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+// import Cookies from "universal-cookie ";
+import { ChannelListContainer, ChannelContainer } from "./components";
+
+const apiKey = "w5z7t2a99tum";
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Chat!</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+
+        <ChannelListContainer />
+      </Chat>
     </div>
   );
 };
