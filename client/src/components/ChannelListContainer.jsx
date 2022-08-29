0f646b20c8bd5a72dd051a6e4b2059e3d1af9692
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-29 13:47:43 +0100

    :art: Sidebar component setup
    configured the side bar component and also set up the assets folder for the entire project

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,24 @@
 import React from "react";
+import { ChannelContainer, useChatContext } from "stream-chat-react";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import Cookies from "universal-cookie";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
+
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__header">
+        <img src={HospitalIcon} alt="hospital" width="30" />
+      </div>
+    </div>
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__header">
+        <img src={LogoutIcon} alt="hospital" width="30" />
+      </div>
+    </div>
+  </div>
+);
 
 const ChannelListContainer = () => {
   return (
