e24007da4678e6f419b88231d117c16a43c463b2
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-30 17:07:41 +0100

    :art: auth component and axios wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 // import Cookies from "universal-cookie ";
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 import "./App.css";
 
 const apiKey = "w5z7t2a99tum";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) {
+    return <Auth />;
+  }
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
+import signinImage from "../assets/signup.jpg";
+
+const Auth = () => {
+  return <div>Auth</div>;
+};
+
+export default Auth;
