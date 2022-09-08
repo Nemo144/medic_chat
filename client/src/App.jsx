4ba08e861e991e7e6cfaf30e14a67877c2cfad14
Nnamdi Okeke <onnamdialex@gmail.com>
2022-09-08 18:14:25 +0100

    :zap: Connect the authTokens

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
-// import Cookies from "universal-cookie ";
+import Cookies from "universal-cookie ";
 import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 import "./App.css";
 
+//if authToken = false, we can get the data by using the cookies
+const cookies = new Cookies();
+
 const apiKey = process.env.STREAM_API_KEY;
+
+//to get the data set in auth.jsx for the cookies
+const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
+if (authToken) {
+  //if we have the authToken, we want to create the user
+  client.connectUser(
+    {
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      fullName: cookies.get("fullName"),
+      image: cookies.get("avatarURL"),
+      hashedPassword: cookies.get("hashedPassword"),
+      phoneNumber: cookies.get("phoneNumber"),
+    },
+    authToken
+  );
+}
 
 const App = () => {
   if (!authToken) {
