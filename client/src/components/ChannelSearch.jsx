4d7d6edfc45cffb56444641d17c3300192bb8917
Nnamdi Okeke <onnamdialex@gmail.com>
2023-03-02 20:47:17 +0100

    :coffin: reading and removing dead code

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useChatContext } from "stream-chat-react";
+import React, { useState } from "react";
+// import { useChatContext } from "stream-chat-react";
 import { SearchIcon } from "../assets";
 
 const ChannelSearch = () => {
